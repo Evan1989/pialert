b3d373ef6331271f9ee6515a73a87601c09341a7
Ivan Shashurin <evan1989@yandex.ru>
2022-07-28 21:39:33 +0300

    Добавил возможность более умного сравнения разных ошибок (подсказки для новых, на основе старых). Улучшил автоматическую склейку текстов. try2

--- src/install/database_1_5.sql
+++ src/install/database_1_5.sql
@@ -1,7 +1,7 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 
-ALTER TABLE `alert_group` ADD `errTextMask` VARCHAR(3000) NULL AFTER `errTextMask`;
+ALTER TABLE `alert_group` ADD `errTextMainPart` VARCHAR(3000) NULL AFTER `errTextMask`;
 
 ALTER TABLE `alert_group` ADD INDEX( errTextMainPart(190) );
 
