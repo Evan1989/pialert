8a8b50f263d0807df4f822e1d514b5164f5cfd0c
Ivan Shashurin <evan1989@yandex.ru>
2023-05-10 22:46:41 +0400

    Небольшое исправление структуры БД

---
+++ src/install/database_1_9.sql
@@ -0,0 +1,8 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+
+ALTER TABLE `alerts` CHANGE `UDSAttributes` `UDSAttributes` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+
+UPDATE `settings` SET `value` = '1.9' WHERE `settings`.`code` = 'DATABASE VERSION';
+
+COMMIT;
