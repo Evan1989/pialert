3af99d47d3fbd278330df7644a878c99a5e6e0b1
tsartemdev <tsartem@yandex.ru>
2023-07-28 15:33:33 +0300

    add comment

--- src/pages/online.php
+++ src/pages/online.php
@@ -11,6 +11,8 @@ require_once(__DIR__."/../autoload.php");
 $authorizationAdmin = new AuthorizationAdmin();
 $authorizationAdmin->ifNotAccessGoErrorPage();
 
+//Тест
+
 $page = new HTMLPageTemplate($authorizationAdmin);
 echo $page->getPageHeader(Text::onlinePageHeader());
 
