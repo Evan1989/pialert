c2c0e707bdc85a40e04f4fde0e4147d8136342e7
tsartemdev <tsartem@yandex.ru>
2023-11-20 20:25:03 +0300

    Add Page to manage SAP PI Business Systems.

--- src/pages/statistics.php
+++ src/pages/statistics.php
@@ -5,8 +5,10 @@ require_once(__DIR__."/../autoload.php");
 use EvanPiAlert\Util\AuthorizationAdmin;
 use EvanPiAlert\Util\DB;
 use EvanPiAlert\Util\essence\PiAlertGroup;
+use EvanPiAlert\Util\essence\PiSystem;
 use EvanPiAlert\Util\HTML\HTMLChart;
 use EvanPiAlert\Util\HTML\HTMLPageTemplate;
+use EvanPiAlert\Util\ManagePiSystem;
 use EvanPiAlert\Util\Settings;
 use EvanPiAlert\Util\Text;
 
@@ -14,13 +16,21 @@ $authorizationAdmin = new AuthorizationAdmin();
 $authorizationAdmin->ifNotAccessGoErrorPage();
 
 // Фильтрация только по одному SAP PI
+
+//var_dump($_GET);
 $choseSystem = $_GET['choseSystem']??'';
-$systemNames = Settings::get(Settings::SYSTEMS_NAMES);
-if ( $systemNames !== false ) {
-    $systemNames = json_decode($systemNames, true);
-} else {
-    $systemNames = array();
+$choseBusinessSystem= $_GET['choseBusinessSystem']??'';
+$piSystems = new ManagePiSystem();
+$piSystemNames=$piSystems->getPiSystems();
+if ( count($piSystemNames) == 0 ) {
+    $piSystemNames = array();
 }
+foreach ($piSystemNames as $piSystem)
+{
+    $systemNames[$piSystem->getSystemName()]=$piSystem->getSID();
+}
+
+
 if ( !isset($systemNames[$choseSystem]) ) {
     $choseSystem = '';
 }
@@ -28,7 +38,7 @@ if ( !isset($systemNames[$choseSystem]) ) {
 $page = new HTMLPageTemplate($authorizationAdmin);
 
 if ( isset($_GET['loadMainStatistics']) ) {
-    if ( $choseSystem ) {
+    if ( !empty($choseSystem)) {
         $query = DB::prepare("
             SELECT count(*) as alert_count, count(DISTINCT a.group_id) as group_count, status
             FROM alerts a LEFT JOIN alert_group g ON a.group_id=g.group_id
@@ -37,7 +47,20 @@ if ( isset($_GET['loadMainStatistics']) ) {
             ORDER BY group_count 
         ");
         $query->execute(array( $choseSystem ));
-    } else {
+    }
+
+    if ($choseBusinessSystem)
+    {
+        $query = DB::prepare("
+            SELECT count(*) as alert_count, count(DISTINCT a.group_id) as group_count, status
+            FROM alerts a LEFT JOIN alert_group g ON a.group_id=g.group_id
+            WHERE  a.fromSystem = ? OR a.toSystem = ?
+            GROUP BY status
+            ORDER BY group_count 
+        ");
+        $query->execute(array( $choseBusinessSystem, $choseBusinessSystem));
+    }
+   else{
         $query = DB::prepare("
             SELECT count(*) as alert_count, count(DISTINCT a.group_id) as group_count, status
             FROM alerts a LEFT JOIN alert_group g ON a.group_id=g.group_id
@@ -46,6 +69,7 @@ if ( isset($_GET['loadMainStatistics']) ) {
         ");
         $query->execute(array());
     }
+
     $data = array();
     $total_alert_count = 0;
     while ($row = $query->fetch()) {
@@ -89,56 +113,85 @@ if ( isset($_GET['loadMainStatistics']) ) {
     exit();
 }
 
+
+
 echo $page->getPageHeader(Text::menuStatistics());
 
-$week_alert = PiAlertGroup::getTotalAlertCount($choseSystem, ONE_WEEK );
-$month_alert = PiAlertGroup::getTotalAlertCount($choseSystem, ONE_MONTH );
+        $week_alert = PiAlertGroup::getTotalAlertCount($choseSystem, $choseBusinessSystem, ONE_WEEK);
+        $month_alert = PiAlertGroup::getTotalAlertCount($choseSystem, $choseBusinessSystem, ONE_MONTH);
+        $week_alertPercent= PiAlertGroup::getAlertPercent($choseSystem, $choseBusinessSystem, ONE_WEEK);
+        $month_alertPercent= PiAlertGroup::getAlertPercent($choseSystem, $choseBusinessSystem, ONE_MONTH);
 
 echo "<div class='card mb-4 shadow'>
         <div class='card-header'>";
 $systemNames[''] = Text::statisticAllSystems();
 foreach ($systemNames as $systemCode => $systemName) {
     echo "<a href='statistics.php?choseSystem=".$systemCode."' class='btn btn-primary ".($choseSystem==$systemCode?'disabled':'')."'>".$systemName."</a> ";
+
+    echo "";
 }
+
+$query = DB::prepare("SELECT code from bs_systems");
+$query->execute();
+echo" <div class='btn-group'>
+  <button type='button' class='btn btn-primary dropdown-toggle' data-bs-toggle='dropdown' aria-expanded='false'>".Text::externalSystems()."</button>
+  <ul class='dropdown-menu'>";
+    while ($row = $query->fetch()) {
+        echo "<li><a class='dropdown-item' href='statistics.php?choseBusinessSystem=".$row['code']."'>".$row['code']."</a></li>";
+}
+echo  "</ul>
+</div>";
 $chart = new HTMLChart();
+
 echo "  </div>
         <div class='card-body overflow-auto'>
             <table class='table table-sm table-hover'>
-                <tbody>
-                      <tr>
-                          <td>".Text::statisticAlert24HourCount()."</td>
-                          <td>".PiAlertGroup::getTotalAlertCount($choseSystem, ONE_DAY )." ".Text::pieces()."</td>
+                <tbody> 
+                <tr>
+                          <td>" . Text::statisticAlert24HourCount() . "</td>
+                          <td>" . PiAlertGroup::getTotalAlertCount($choseSystem, $choseBusinessSystem, ONE_DAY) . " " . Text::pieces() . "</td>
                       </tr>
                       <tr>
-                            <td>".Text::statisticAlertTodayChart()."</td>
-                            <td>".$chart->getHourAlertsChart( $choseSystem )."</td>
+                            <td>" . Text::statisticAlertTodayChart() . "</td>
+                            <td>" . $chart->getHourAlertsChart($choseSystem, $choseBusinessSystem) . "</td>
                       </tr>
                       <tr>
-                          <td>".Text::statisticAlertWeekCount()."</td>
-                          <td>".$week_alert." ".Text::pieces()." ≈ ".round10($week_alert/7).' '.Text::perDay()."</td>
+                          <td>" . Text::statisticAlertWeekCount() . "</td>
+                          <td>" . $week_alert . " " . Text::pieces() . " ≈ " . round10($week_alert / 7) . ' ' . Text::perDay() . "</td>
                       </tr>
                       <tr>
-                          <td>".Text::statisticAlertMonthCount()."</td>
-                          <td>".$month_alert." ".Text::pieces()." ≈ ".round10($month_alert/30.5).' '.Text::perDay()."</td>
+                          <td>" . Text::statisticAlertMonthCount() . "</td>
+                          <td>" . $month_alert . " " . Text::pieces() . " ≈ " . round10($month_alert / 30.5) . ' ' . Text::perDay() . "</td>
                       </tr>
                       <tr>
-                            <td>".Text::statisticAlertMonthChart()."</td>
-                            <td>".$chart->getDailyAlertsChart( $choseSystem )."</td>
+                            <td>" . Text::statisticAlertMonthChart() . "</td>
+                            <td>" . $chart->getDailyAlertsChart($choseSystem, $choseBusinessSystem) . "</td>
                       </tr>
                       <tr>
-                          <td>".Text::statisticAlertTotalCount()."</td>
-                          <td>".PiAlertGroup::getTotalAlertCount($choseSystem)." ".Text::pieces()."</td>
-                      </tr>";
-        echo "    </tbody>
+                          <td>" . Text::statisticAlertTotalCount() . "</td>
+                          <td>" . PiAlertGroup::getTotalAlertCount($choseSystem, $choseBusinessSystem) . " " . Text::pieces() . "</td>
+                      </tr>
+                      <tr>
+                          <td>" . Text::statisticAlertTotalPercent() . "</td>
+                          <td>" . PiAlertGroup::getAlertPercent($choseSystem, $choseBusinessSystem) . "%</td>
+                      </tr>  
+                      <tr>
+                          <td>" . Text::statisticAlertWeekPercent() . "</td>
+                          <td>" . $week_alertPercent . "%  ≈ " . round10($week_alertPercent / 7) . ' ' . Text::perDay() . "</td>
+                      </tr> 
+                      <tr>
+                          <td>" . Text::statisticAlertMonthPercent() . "</td>
+                          <td>" . $month_alertPercent . "%  ≈ " . round10($month_alertPercent / 7) . ' ' . Text::perDay() . "</td>
+                      </tr>     
+                      </tbody>
             </table>
-              
             <div class='main-statistic'></div>
         </div>
     </div>";
 
 $additionalScript = "<script type='text/javascript'>
         $(document).ready(function() {
-            $.get( 'statistics.php?loadMainStatistics=1&choseSystem=".$choseSystem."', function( data ) {
+            $.get( 'statistics.php?loadMainStatistics=1&choseSystem=".$choseSystem.".&choseBusinessSystem=".$choseBusinessSystem."', function( data ) {
                 $('.main-statistic').html( data );
             });
         })
